c33f993f69b306460b98271694980c9889caa291
ramroumette <mr.toumirami@outlook.fr>
2025-05-11 01:04:57 +0100

    Add embla-carousel and implement AppCarousel component with navigation

---
+++ app/swipe/page.tsx
@@ -0,0 +1,11 @@
+import { AppCarousel } from "@/components/app-carousel";
+
+export default function SwipePage() {
+    return (
+        <div className=" flex tems-center justify-center h-screen">
+          
+                <AppCarousel/>
+            </div>
+   
+    )
+}
