d6c2c7336cbdbbdcdf11850ac83f747a93c4d72d
ramroumette <mr.toumirami@outlook.fr>
2025-05-16 20:17:49 +0100

    feat: replace CommandAll with CommandPalette and refactor layout for improved UI

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,9 @@ import { AppSidebar } from "@/components/app-sidebar";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
+import { ModeToggle } from "@/components/mode-toggle";
+import { CommandPalette } from "@/components/command-palette";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -22,6 +25,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               <main>{children}</main>
             </div>
           </SidebarProvider>
+          <div className="fixed top-4 right-4 z-50">
+            <div className="flex items-center gap-2">
+              <CommandPalette />
+              <ModeToggle />
+            </div>
+          </div>
         </ThemeProvider>
       </body>
     </html>
