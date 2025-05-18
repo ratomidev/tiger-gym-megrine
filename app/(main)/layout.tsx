cbd25e6d367926b8343ba274d7e5e905a6adba8b
ramroumette <mr.toumirami@outlook.fr>
2025-05-18 09:24:11 +0100

    refactor: restructure Layout component for improved readability organization and styling

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,38 +1,31 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { NavigationSidebar } from "@/components/navigation-sidebar";
 
-import { ThemeProvider } from "@/components/theme-provider";
-
-import { ModeToggle } from "@/components/mode-toggle";
 import { CommandPalette } from "@/components/command-palette";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <SidebarProvider>
-            <NavigationSidebar />
-            <div className="relative w-full h-full">
-              <div className="fixed top-4 z-50 ">
-                <SidebarTrigger />
-              </div>
-              <main className="w-full h-screen pt-30">{children}</main>
-            </div>
-          </SidebarProvider>
-          <div className="fixed top-4 right-4 z-50">
-            <div className="flex items-center gap-2">
-              <CommandPalette />
-              <ModeToggle />
-            </div>
+    <div className="flex">
+      <SidebarProvider>
+        <header className="fixed top-4 right-4 z-50">
+          <div className="flex items-center gap-2">
+            <CommandPalette />
+            <ModeToggle />
           </div>
-        </ThemeProvider>
-      </body>
-    </html>
+        </header>
+
+        <aside>
+          <NavigationSidebar />
+        </aside>
+
+        <main className="flex-1 relative">
+          <div className="absolute top-4 left-4 z-50">
+            <SidebarTrigger />
+          </div>
+          <div className="w-full h-screen pt-30">{children}</div>
+        </main>
+      </SidebarProvider>
+    </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
 
-import "./globals.css"
-
- 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-    {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
