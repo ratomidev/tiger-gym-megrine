6534efc2d637d119ec17ceb2c5466705d6437141
ramroumette <mr.toumirami@outlook.fr>
2025-07-04 16:57:05 +0100

    feat: add viewport configuration to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import { AuthProvider } from "@/context/AuthContext";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Viewport } from "next";
 import "./globals.css";
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
